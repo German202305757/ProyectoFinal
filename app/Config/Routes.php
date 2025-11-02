420c2a51616834f94bdda66a6e7f6ece1a4f2a29
German202305757 <germanalejandro2004paz@gmail.com>
2025-11-02 08:39:38 -0600

    Archivo Routes modificado para las rutas de Usuarios y agregar-usuario

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
+$routes->get('usuarios','UsuariosController::index');
 //Agregar
-
+$routes->post('agregar_usuario','UsuariosController::agregarUsuario');
 
 //Eliminar
 
