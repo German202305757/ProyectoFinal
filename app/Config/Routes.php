725ce004952306a6261b017e176ea885da1af5cb
German202305757 <germanalejandro2004paz@gmail.com>
2025-11-02 09:19:44 -0600

    Ruta de buscar usuario implementada

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,7 +14,7 @@ $routes->get('/', 'Home::index');
 $routes->get('eliminar_usuario/(:num)','UsuariosController::eliminarUsuario/$1');
 
 //Buscar
-
+$routes->get('buscar_usuario/(:num)','UsuariosController::buscarUsuario/$1');
 
 //Modificar
 $routes->post('modificar_usuario', 'UsuariosController::modificarUsuario');
