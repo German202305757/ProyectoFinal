f8ef5ab6627ae0b8a854bb4875b6d0c25e6ea325
German202305757 <germanalejandro2004paz@gmail.com>
2025-11-01 18:49:58 -0600

    Integración de la carpeta del ProyectoFinal

---
+++ app/Config/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+//Agregar
+
+
+//Eliminar
+
+
+//Buscar
+
+
+//Modificar
