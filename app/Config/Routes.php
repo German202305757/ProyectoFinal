9c35a53ea967d3f745dd4b10eb0cd674640210dc
Lennin1984 <lennincardona8421@gmail.com>
2025-11-05 07:35:16 -0600

    Iniciamos la eliminación de las mascotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,7 +11,7 @@ $routes->get('mascotas','MascotasController::index');
 $routes->post('agregar_mascotas','MascotasController::agregarMascotas');
 
 //Eliminar
-
+$routes->get('eliminar_mascotas/(:num)','MascotasController::eliminarMascotas/$1');
 
 //Buscar
 
