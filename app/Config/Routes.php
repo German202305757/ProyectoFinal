778f0c79eb20878d075432402574dc1ddfa30d66
Lennin1984 <lennincardona8421@gmail.com>
2025-11-05 08:50:07 -0600

    Iniciamos con la visualización de mostrar toda la información de las citas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,12 +8,15 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 //Agregar
-
+$routes->get('citas','CitasController::index');
+$routes->post('agregar_citas','CitasController::agregarCitas');
 
 //Eliminar
-
+$routes->get('eliminar_citas/(:num)','CitasController::eliminarCitas/$1');
 
 //Buscar
-
+$routes->get('buscar_citas/(:num)','CitasController::buscarCitas/$1');
 
 //Modificar
+$routes->post('modificar_citas','CitasController::modificarCitas');
+
