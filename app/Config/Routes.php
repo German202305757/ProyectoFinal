48b26fad730531850379a2005f14ed9cbe879218
Lennin1984 <lennincardona8421@gmail.com>
2025-11-05 07:53:19 -0600

    Iniciamos con la busqueda de las mascotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,12 +8,14 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 //Agregar
-
+$routes->get('/', 'Home::index');
+$routes->get('mascotas','MascotasController::index');
+$routes->post('agregar_mascotas','MascotasController::agregarMascotas');
 
 //Eliminar
-
+$routes->get('eliminar_mascotas/(:num)','MascotasController::eliminarMascotas/$1');
 
 //Buscar
-
+$routes->get('buscar_mascotas/(:num)','MascotasController::buscarMascotas/$1');
 
 //Modificar
