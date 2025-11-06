429e96aa9ddf47a7caee77b75d3bfa3585590faa
pablosontay <pablosontay1177@gmail.com>
2025-11-06 17:11:46 -0600

    se mostraran todos lo elementos de historial medico

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,10 +8,11 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 //Agregar
+$routes->post('agregarregistro','HistorialMedicoController::agregarregistro');
 
 
 //Eliminar
-
+$routes->get('eliminarregistro/(:num)','HistorialMedicoController::eliminarregistro/$1');
 
 //Buscar
 
