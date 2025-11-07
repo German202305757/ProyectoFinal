a1aa39558058d24111beb1b52e4ed5dece5d2d5d
pablosontay <pablosontay1177@gmail.com>
2025-11-06 18:13:57 -0600

    se creo la rama buscar solicitud y se subio el archivo

---
+++ app/Controllers/SolicitudesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\SolicitudesModel;
+
+class SolicitudesController extends BaseController
+{
+    public function index(): string
+    {        
+        $solicitud= new SolicitudesModel();
+        $datos['datos']= $solicitud->findAll();
+        return view('solicitudes', $datos);
+    }
+    public function agregarSolicitud()
+    {        
+        $solicitud = new SolicitudesModel();
+        $datos=[
+            'id_solicitud'=> $this-> request->getPost('txt_id_solicitud'),
+            'id_adoptante'=> $this-> request->getPost('txt_id_adoptante'),
+            'id_animal'=> $this-> request->getPost('txt__id_animal'),
+            'estado_solicitud'=> $this-> request->getPost('txt_estado'),
+            'fecha_solicitud'=> $this-> request->getPost('txt_fecha'),
+            'comentario'=> $this-> request->getPost('txt_comentario'),
+
+        ];
+        print_r($datos);
+        $solicitud->insert($datos);
+        return $this->index();
+    }
+
+   public function buscarSolicitud($codigo){
+        echo "codigo seleccionado para busqueda: ". $codigo;
+        $solicitud = new SolicitudesModel();  
+        $datos['datos']= $solicitud->where('id_solicitud',$codigo)->first();
+        return view('form_editar_solicitud',$datos);
+    }
+}
